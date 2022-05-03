4f4f3324454b0ad46612bd4f5a69740cd2c9afde
Shaan Sundar <shaan.idtindia@gmail.com>
2022-05-03 13:31:20 +0530

    v1.0.6 - Add Officials input, new background

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -7,30 +7,39 @@ import { ContractService } from 'src/app/shared/services/contract.service';
   selector: 'app-donate',
   templateUrl: './donate.component.html',
   styles: [
-  ]
+  ],
 })
 export class DonateComponent implements OnInit {
   private value: Number;
-  constructor(private contractAccess : ContractService, private router: Router) { 
-    this.value=0;
+  private addressValue: String = "";
+  constructor(private contractAccess: ContractService, private router: Router) {
+    this.value = 0;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  public updateValue(e: any){
+  public updateValue(e: any) {
     this.value = e.target.value;
   }
+  public updateValueAddress(e: any) {
+    this.addressValue = e.target.value;
+  }
 
-  async Donate(){
+  async Donate() {
     let x = await this.contractAccess.getContract();
     let y = await x.donate({
-      value: ethers.utils.parseEther(String(this.value))
+      value: ethers.utils.parseEther(String(this.value)),
     });
     await y.wait();
-    alert("Thank you for the Donation!")
+    alert('Thank you for the Donation!');
     window.location.reload();
     this.router.navigate(['/']);
   }
-
+  async Assign() {
+    let x = await this.contractAccess.getContract();
+    let y = await x.addGovOfficial(this.addressValue);
+    await y.wait();
+    alert('Address Added as Official!');
+    window.location.reload();
+  }
 }
