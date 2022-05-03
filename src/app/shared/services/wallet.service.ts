9c7864a428d24c826fbe00b501cbfaf557ebadc8
Shaan Sundar <shaan.idtindia@gmail.com>
2022-05-03 16:44:18 +0530

    v1.1.1 - Added warnings and Elaboration

--- src/app/shared/services/wallet.service.ts
+++ src/app/shared/services/wallet.service.ts
@@ -20,14 +20,19 @@ export class WalletService {
       if (typeof window.ethereum == 'undefined') {
         alert('MetaMask is not installed!');
       }
+
       this.provider = new ethers.providers.Web3Provider(window.ethereum);
       this.signer = this.provider.getSigner();
+      if(await this.signer.getChainId() !== 80001){
+        alert("Please Change your network to Mumbai testnet! RPC: https://rpc-mumbai.maticvigil.com, ChainID: 80001");
+      }else{
       this.walletAddress = await this.signer.getAddress();
       this.balance = await this.signer.getBalance();
       this.slicedBalance = Number(
         String((await this.signer.getBalance()) / 10 ** 18).slice(0, 6)
       );
       this.isWalletConnected = true;
+      }
     } else {
       alert('Please install Metamask!');
     }
